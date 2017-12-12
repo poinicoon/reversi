d782902dc6ec04831c2e720826fc2a797e7dddd9
poini_coon <zxxzxxxxz@gmail.com>
2017-12-13 03:17:51 +0900

    fix

--- loop.sh
+++ loop.sh
@@ -6,6 +6,9 @@ for ((i=0 ; i<epoch ; i++))
 do
 	echo -n "loop: "
 	echo $epoch
-	~/venv_reversi_learn/bin/python3 start.py
-	~/venv_reversi_learn/bin/python3 train.py
+
+	~/venv/bin/python3 make_datasets.py
+	~/venv/bin/python3 make_testdata_from_datasets.py
+	~/venv/bin/python3 train.py
+	
 done
