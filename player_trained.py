cde7640aba032a2c90193e1f7f6681665d9e45c3
poini_coon <zxxzxxxxz@gmail.com>
2017-12-19 13:27:19 +0900

    fix

--- player_trained.py
+++ player_trained.py
@@ -1,11 +1,21 @@
+import json
 import numpy as np
+from keras.models import load_model
 
 import player_base
+from funcs import GetField3dimOnehot
+
+config = json.load(open("config.json", "r"))
 
 
 class PlayerTrained(player_base.PlayerBase):
-    def execute_(self, raw_field: np.ndarray) -> np.ndarray:
-        put_coord_list = []  # type: typing.List[np.ndarray]
+    def execute_(self, field: np.ndarray) -> np.ndarray:
+        coord_list = self.model.predict(GetField3dimOnehot(field))  # type: np.ndarray
+        coord_avg = np.average(coord_list)
+        coord_list = [i for i in coord_list[0] if coord_avg < i]
+        return coord_list[np.random.randint(len(coord_list))]
 
-    def __init__(self, player_num: int, player_name="Trained"):
-        super(PlayerTrained, self).__init__(player_num, player_name=player_name)
+    def __init__(self, player_num: int, field_size: np.ndarray, player_name="Trained"):
+        super(PlayerTrained, self).__init__(player_num, field_size, player_name=player_name)
+
+        self.model = load_model(config["model_path"])
