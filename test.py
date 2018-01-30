660553c88876e8d7126812c6b856781bedc16b2a
poini_coon <zxxzxxxxz@gmail.com>
2018-01-31 02:38:32 +0900

    fix

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+import os
 import numpy as np
 import keras
 
 import config
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 input = np.array([[[[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]],
                    [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]],
@@ -10,11 +13,21 @@ input = np.array([[[[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0
                    [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]],
                    [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]]]])
 
-print("Input")
+print("Input: ")
 for col in range(input.shape[1]):
+    if col == 0:
+        print(" ",end=" | ")
+        for row in range(input.shape[2]):
+            print(row, end=" ")
+        print()
+        print("-", end="-|-")
+        for row in range(input.shape[2]):
+            print("-", end="-")
+        print()
+    print(col, end=" | ")
     for row in range(input.shape[2]):
         if input[0, col, row, 0] == 1:
-            print("-", end=" ")
+            print("*", end=" ")
         elif input[0, col, row, 1] == 1:
             print("1", end=" ")
         elif input[0, col, row, 2] == 1:
@@ -26,12 +39,8 @@ print()
 model = keras.models.load_model(config.ModelPath)
 
 output = model.predict(input)
-
-output = output.reshape(6, 6)
-
+output = output.reshape(input.shape[1:3])
 output = np.round(output, 4)
 
-for col in range(output.shape[0]):
-    for row in range(output.shape[1]):
-        print(output[col, row], end=" ")
-    print()
+print("Output: ")
+print(output)
