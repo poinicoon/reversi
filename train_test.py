3fd93863fc2cd1834c48316ddc5b41c96330ce38
poini_coon <zxxzxxxxz@gmail.com>
2018-01-30 18:02:51 +0900

    fix

--- train_test.py
+++ train_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import keras
+import tkinter
 
 import config
 
@@ -12,21 +13,16 @@ input = np.array([[[[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0
 
 print("Input")
 for col in range(input.shape[1]):
-    if col == 0:
-        print("& 0 & 1 & 2 & 3 & 4 & 5 \\\\ \\hline \\hline")
-    print(col, end="")
-    print(" & ", end="")
     for row in range(input.shape[2]):
         if 0 < row:
-            print(" & ", end="")
+            print(", ", end="")
         if input[0, col, row, 0] == 1:
             print("-", end="")
         elif input[0, col, row, 1] == 1:
             print("1", end="")
         elif input[0, col, row, 2] == 1:
             print("2", end="")
-        print(" ", end="")
-    print(" \\\\ \\hline")
+    print()
 
 print()
 
@@ -37,15 +33,11 @@ output = model.predict(input)
 output = output.reshape(6, 6)
 
 for col in range(output.shape[0]):
-    if col == 0:
-        print("& 0 & 1 & 2 & 3 & 4 & 5 \\\\ \\hline \\hline")
-    print(col, end="")
-    print(" & ", end="")
     for row in range(output.shape[1]):
         if 0 < row:
-            print(" & ", end="")
+            print(", ", end="")
         print(output[col, row], end="")
-    print(" \\\\ \\hline")
+    print()
 
 print()
 
