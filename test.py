89ec4707427c4e5b70b739ac825b8c1c9b9887ac
poini_coon <zxxzxxxxz@gmail.com>
2018-01-31 16:00:03 +0900

    fix

--- test.py
+++ test.py
@@ -3,35 +3,26 @@ import numpy as np
 import keras
 
 import config
+from funcs import GetField3dimOnehot
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-input = np.array([[[[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]],
-                   [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]],
-                   [[1, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1], [1, 0, 0], [1, 0, 0]],
-                   [[1, 0, 0], [1, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 0, 0]],
-                   [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]],
-                   [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]]]])
-
-input2 = np.zeros(input.shape[1:3])
-for col in range(input2.shape[0]):
-    for row in range(input2.shape[1]):
-        if input[0, col, row, 0] == 1:
-            continue
-        elif input[0, col, row, 1] == 1:
-            input2[col, row] = 1
-        elif input[0, col, row, 2] == 1:
-            input2[col, row] = 2
+input = np.array([[0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0],
+                  [0, 0, 1, 2, 0, 0],
+                  [0, 0, 2, 1, 0, 0],
+                  [0, 0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0, 0]], dtype=int)
 
 model = keras.models.load_model(config.ModelPath)
 
-output = model.predict(input)
-output = output.reshape(input.shape[1:3])
+output = model.predict(GetField3dimOnehot(input - 1).reshape([1, 6, 6, 3]))
+output = output.reshape(input.shape)
 output = np.round(output, 4)
 
 print()
 print("Input: ")
-print(input2)
+print(input)
 print()
 
 print("Output: ")
