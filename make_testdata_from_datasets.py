57e256d2ac0920ad40f91abe8ac02850e418127e
poini_coon <zxxzxxxxz@gmail.com>
2018-01-31 16:38:06 +0900

    fix

--- make_testdata_from_datasets.py
+++ make_testdata_from_datasets.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 
 from config import XTrainPath, YTrainPath, XTestPath, YTestPath
-from funcs import GetTestCoord
+from funcs import GetTestCoord, Softmax
 
 # データセット読み込み
 x_train = np.load(XTrainPath)
@@ -14,7 +14,7 @@ y_test = []
 
 for i in range(len(x_train)):
     sys.stdout.write("\r" + "make_testdata: " + str(i + 1) + "/" + str(len(x_train)))
-    y_test.append(GetTestCoord(x_train[i]))
+    y_test.append(Softmax(GetTestCoord(x_train[i])))
 
 print()
 
