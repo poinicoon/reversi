04d5f707d264fdc39061610598ed395e4fb0f960
poini_coon <zxxzxxxxz@gmail.com>
2018-01-31 15:44:47 +0900

    fix

--- test.py
+++ test.py
@@ -13,29 +13,6 @@ input = np.array([[[[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0
                    [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]],
                    [[1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0]]]])
 
-print("Input: ")
-for col in range(input.shape[1]):
-    if col == 0:
-        print(" ",end=" | ")
-        for row in range(input.shape[2]):
-            print(row, end=" ")
-        print()
-        print("-", end="-|-")
-        for row in range(input.shape[2]):
-            print("-", end="-")
-        print()
-    print(col, end=" | ")
-    for row in range(input.shape[2]):
-        if input[0, col, row, 0] == 1:
-            print("*", end=" ")
-        elif input[0, col, row, 1] == 1:
-            print("1", end=" ")
-        elif input[0, col, row, 2] == 1:
-            print("2", end=" ")
-    print()
-
-print()
-
 input2 = np.zeros(input.shape[1:3])
 for col in range(input2.shape[0]):
     for row in range(input2.shape[1]):
@@ -46,13 +23,17 @@ for col in range(input2.shape[0]):
         elif input[0, col, row, 2] == 1:
             input2[col, row] = 2
 
-print(input2)
-
 model = keras.models.load_model(config.ModelPath)
 
 output = model.predict(input)
 output = output.reshape(input.shape[1:3])
 output = np.round(output, 4)
 
+print()
+print("Input: ")
+print(input2)
+print()
+
 print("Output: ")
 print(output)
+print()
