a88b229bf2f8a330510e383db902434940b149e0
poini_coon <zxxzxxxxz@gmail.com>
2018-02-22 16:03:19 +0900

    fix

--- train.py
+++ train.py
@@ -1,56 +1,8 @@
-import os
 import sys
 import numpy as np
-from keras.layers import Activation, BatchNormalization, Conv2D, Flatten, InputLayer, MaxPooling2D
-from keras.models import Sequential, load_model
+from keras.models import load_model
 
 field_size = (6, 6)
-
-'''
-def make_model():
-    model = Sequential()
-    model.add(InputLayer(input_shape=(self.x_train.shape[1:])))
-    model.add(Conv2D(64, self.x_train.shape[1:3], padding='same', activation='relu'))
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(1, (1, 1), padding='same', activation='relu', use_bias=True))
-    model.add(Flatten())
-    model.add(Activation('relu'))
-    model.add(Activation('softmax'))
-
-    model.compile(loss='categorical_crossentropy', optimizer="SGD", metrics=['accuracy'])
-
-    return model
-'''
-
-
-def make_model():
-    model = Sequential()
-    model.add(InputLayer(input_shape=(field_size[0], field_size[1], 3)))
-
-    model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(Activation('relu'))
-
-    model.add(BatchNormalization())
-
-    model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(Activation('relu'))
-
-    model.add(BatchNormalization())
-
-    model.add(Conv2D(1, (1, 1), padding='same'))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(1, 1), padding='same'))
-
-    model.add(BatchNormalization())
-
-    model.add(Flatten())
-
-    model.add(Activation('relu'))
-    model.add(Activation('softmax'))
-
-    model.compile(loss='categorical_crossentropy', optimizer="SGD", metrics=['accuracy'])
-
-    return model
 
 
 def train(model, x_train, y_train, x_test, y_test):
@@ -67,17 +19,8 @@ def train(model, x_train, y_train, x_test, y_test):
     return model
 
 
-if __name__ == "__main__":
-    model_path = sys.argv[1]
-    x_train_path = sys.argv[2]
-    y_train_path = sys.argv[3]
-    x_test_path = sys.argv[4]
-    y_test_path = sys.argv[5]
-
-    if os.path.exists(model_path):
-        model = load_model(model_path)
-    else:
-        model = make_model()
+def main(model_path, x_train_path, y_train_path, x_test_path, y_test_path):
+    model = load_model(model_path)
 
     x_train = np.load(x_train_path)
     y_train = np.load(y_train_path)
@@ -86,3 +29,13 @@ if __name__ == "__main__":
 
     model = train(model, x_train, y_train, x_test, y_test)
     model.save(model_path)
+
+
+if __name__ == "__main__":
+    model_path = sys.argv[1]
+    x_train_path = sys.argv[2]
+    y_train_path = sys.argv[3]
+    x_test_path = sys.argv[4]
+    y_test_path = sys.argv[5]
+
+    main(model_path, x_train_path, y_train_path, x_test_path, y_test_path)
