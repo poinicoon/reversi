727ddf5fd573e53895035df2639fde03dac205d1
poini_coon <zxxzxxxxz@gmail.com>
2018-02-22 21:53:20 +0900

    fix

--- generate_model.py
+++ generate_model.py
@@ -6,46 +6,18 @@
 
 import sys
 from keras.models import Sequential, InputLayer
-from keras.layers import Conv2D, Activation, BatchNormalization, MaxPooling2D, Flatten
+from keras.layers import Conv2D, Activation, Flatten
 
 field_size = (6, 6)
 
 
 def make_model():
-    """
     model = Sequential()
-    model.add(InputLayer(input_shape=(self.x_train.shape[1:])))
-    model.add(Conv2D(64, self.x_train.shape[1:3], padding='same', activation='relu'))
+    model.add(InputLayer(input_shape=(6, 6)))
+    model.add(Conv2D(64, (6, 6), padding='same', activation='relu'))
     model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
     model.add(Conv2D(1, (1, 1), padding='same', activation='relu', use_bias=True))
     model.add(Flatten())
-    model.add(Activation('relu'))
-    model.add(Activation('softmax'))
-
-    model.compile(loss='categorical_crossentropy', optimizer="SGD", metrics=['accuracy'])
-    """
-
-    model = Sequential()
-    model.add(InputLayer(input_shape=(field_size[0], field_size[1], 3)))
-
-    model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(Activation('relu'))
-
-    model.add(BatchNormalization())
-
-    model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(Activation('relu'))
-
-    model.add(BatchNormalization())
-
-    model.add(Conv2D(1, (1, 1), padding='same'))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(1, 1), padding='same'))
-
-    model.add(BatchNormalization())
-
-    model.add(Flatten())
-
     model.add(Activation('relu'))
     model.add(Activation('softmax'))
 
