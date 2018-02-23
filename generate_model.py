aecc095bd26300aafc9d6ed96a07ebe1207c5924
poini_coon <zxxzxxxxz@gmail.com>
2018-02-23 15:59:51 +0900

    fix

--- generate_model.py
+++ generate_model.py
@@ -11,13 +11,13 @@ field_size = (6, 6)
 
 def make_model():
     model = Sequential()
-    model.add(InputLayer(input_shape=(field_size[0], field_size[1], 3)))
-    model.add(Conv2D(64, (5, 5), padding='same', activation='relu'))
-    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-    model.add(Conv2D(1, (1, 1), padding='same', activation='relu', use_bias=True))
-    model.add(Flatten())
-    model.add(Activation('relu'))
-    model.add(Activation('softmax'))
+    model.add(InputLayer(input_shape=(field_size[0], field_size[1], 3)))  # (6, 6, 3) -> (6, 6, 3)
+    model.add(Conv2D(64, (5, 5), padding='same', activation='relu'))  # (6, 6, 3) -> (6, 6, 64)
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))  # (6, 6, 64) -> (6, 6, 64)
+    model.add(Conv2D(1, (1, 1), padding='same', activation='relu', use_bias=True))  # (6, 6, 64) -> (6, 6, 1)
+    model.add(Flatten())  # (6, 6, 1) -> (36)
+    model.add(Activation('relu'))  # (36) -> (36)
+    model.add(Activation('softmax'))  # (36) -> (36)
 
     model.compile(loss='categorical_crossentropy', optimizer="SGD", metrics=['accuracy'])
 
